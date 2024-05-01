37d1a496d9799abbc44e595c3d4c1d700069e425
Mariana Perna <marianaperna@ua.pt>
2024-05-01 16:43:30 +0100

    estrutura inicial

---
+++ cliniconnect/src/main/java/pt/ua/deti/tqs/cliniconnect/CliniconnectApplication.java
@@ -0,0 +1,13 @@
+package pt.ua.deti.tqs.cliniconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CliniconnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CliniconnectApplication.class, args);
+	}
+
+}
---
+++ cliniconnect/src/test/java/pt/ua/deti/tqs/cliniconnect/CliniconnectApplicationTests.java
@@ -0,0 +1,13 @@
+package pt.ua.deti.tqs.cliniconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CliniconnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
