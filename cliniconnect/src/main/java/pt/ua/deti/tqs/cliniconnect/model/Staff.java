9891d8938c2fe00fa8e06f2af1bcba6d99f4c444
Mariana Perna <marianaperna@ua.pt>
2024-05-01 19:48:54 +0100

    models orientados

---
+++ cliniconnect/src/main/java/pt/ua/deti/tqs/cliniconnect/model/Staff.java
@@ -0,0 +1,69 @@
+package pt.ua.deti.tqs.cliniconnect.model;
+
+import jakarta.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "staff")
+public class Staff {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "staff_id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "role")
+    private String role;
+
+    @ManyToOne
+    @JoinColumn(name = "hospital_id")
+    private Hospital hospital;
+
+    // Assuming staff can manage appointments
+    @OneToMany(mappedBy = "managedBy")
+    private Set<Appointment> appointments = new HashSet<>();
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public Hospital getHospital() {
+        return hospital;
+    }
+
+    public void setHospital(Hospital hospital) {
+        this.hospital = hospital;
+    }
+
+    public Set<Appointment> getAppointments() {
+        return appointments;
+    }
+
+    public void setAppointments(Set<Appointment> appointments) {
+        this.appointments = appointments;
+    }
+}
