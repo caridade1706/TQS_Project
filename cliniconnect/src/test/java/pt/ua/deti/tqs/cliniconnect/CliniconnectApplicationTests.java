78ff5d727744d35edc078f097672f945500f686f
Roberto Castro <robertorcastro@ua.pt>
2024-05-12 15:23:29 +0100

    update

--- cliniconnect/src/test/java/pt/ua/deti/tqs/cliniconnect/CliniconnectApplicationTests.java
+++ cliniconnect/src/test/java/pt/ua/deti/tqs/cliniconnect/CliniconnectApplicationTests.java
@@ -1,11 +1,9 @@
 package pt.ua.deti.tqs.cliniconnect;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-@Disabled
 class CliniconnectApplicationTests {
 
 	@Test
