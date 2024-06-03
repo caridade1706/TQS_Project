e6a4e8ca39af713ee8f5ddd2a2f3a8e6fcc6b563
Roberto Castro <robertorcastro@ua.pt>
2024-06-03 23:34:32 +0100

    update

--- frontend/staff_website/src/components/AddDoctorForm.js
+++ frontend/staff_website/src/components/AddDoctorForm.js
@@ -33,7 +33,7 @@ function AddDoctorForm() {
     
     const fetchHospitals = async () => {
         try {
-            const response = await axios.get('http://localhost:8080/api/hospitals/');
+            const response = await axios.get(process.env.REACT_APP_API_URL + 'hospitals/');
             setHospitals(response.data);
         } catch (error) {
             console.error('Error fetching hospitals:', error);
